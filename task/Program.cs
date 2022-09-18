141cbb68b54d2cf4f3a2f1d651b0340bfebdda27
DanilaB <bleskind54@gmail.com>
2022-09-18 13:22:59 +0700

    codefile

---
+++ task/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+string[] str = new string[3];
+int count = 0;
+int j = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    str[i] = Console.ReadLine();
+    if (str[i].Length <= 3)
+        count ++;
+}
+string[] resstr = new string[count];
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i].Length <= 3 )
+    {
+        resstr[j] = str[i];
+        j++;
+    }
+}
+foreach (var el in resstr)
+    Console.WriteLine(el);
